017ebefa4fe24404237c2f7f234b90877054d238
frtncr <firattuncer2@gmail.com>
2020-10-12 01:11:29 +0300

    tests mock object change

--- IncomeTaxCalculatorTest/TaxCalculatorControllerTests.cs
+++ IncomeTaxCalculatorTest/TaxCalculatorControllerTests.cs
@@ -8,25 +8,24 @@ namespace IncomeTaxCalculatorTest
 {
     public class TaxCalculatorControllerTests
     {
-        // private TaxCalculatorController _sut;
-        //
-        // public TaxCalculatorControllerTests()
-        // {
-        //     _sut = new TaxCalculatorController();
-        // }
+        private readonly TaxCalculatorController _sut;
+        private readonly Mock<ITaxCalculatorService> _mockTaxCalculatorService;
+        
+        public TaxCalculatorControllerTests()
+        {
+            _mockTaxCalculatorService = new Mock<ITaxCalculatorService>();
+            _sut = new TaxCalculatorController(_mockTaxCalculatorService.Object);
+        }
 
         [Fact]
         public void CalculateIncomeTaxAction_ShouldNotReturn_Null()
         {
             // Arrange
-            var mockTaxCalculatorService = new Mock<ITaxCalculatorService>();
-            mockTaxCalculatorService.Setup(x => x.Calculate("TR", 32000m))
+            _mockTaxCalculatorService.Setup(x => x.Calculate("TR", 32000m))
                 .Returns(5300m);
             
-            var sut = new TaxCalculatorController(mockTaxCalculatorService.Object);
-
             // Act
-            var result = sut.CalculateIncomeTax("TR", 32000m) as OkObjectResult;
+            var result = _sut.CalculateIncomeTax("TR", 32000m) as OkObjectResult;
 
             // Assert
             Assert.NotNull(result);
